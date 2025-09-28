c95d572ec936cddbcbe9715ca7e9f2552b9f1ac6
haok1402 <haok1402@gmail.com>
2025-09-27 21:58:56 -0400

    run the evaluation on HELMET after long-context extension

--- scripts/run_eval_qwen3_64K.sh
+++ scripts/run_eval_qwen3_64K.sh
@@ -7,8 +7,8 @@ source $HOME/miniconda3/etc/profile.d/conda.sh
 conda activate HELMET
 
 # Specify the model checkpoint and output directory.
-MODEL_NAME_OR_PATH=workspace/checkpoints/lcft_Qwen3-0.6B_long-context-65536_ProLong64KMix_bsz64_steps1000_lr1e-5_warmup0.1_rope4000000/checkpoint-500
-OUTPUT_DIR=workspace/outputs/Qwen3-0.6B-64K-rope4M/checkpoint-500/short
+MODEL_NAME_OR_PATH=workspace/checkpoints/lcft_Qwen3-0.6B_long-context-65536_ProLong64KMix_bsz64_steps1000_lr1e-5_warmup0.1_rope4000000/checkpoint-1000/
+OUTPUT_DIR=workspace/outputs/Qwen3-0.6B-64K-rope4M/checkpoint-1000/short
 
 # Make use of reduced sample sizes for quick validation.
 CONFIG=configs/recall_short.yaml; MAX_TEST_SAMPLES=25
